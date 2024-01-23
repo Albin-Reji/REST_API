fa293c1bcb2a99750d6ca54274bcacffa4a385f8
Albin Reji <albinrejim30@gmail.com>
2024-01-24 00:08:11 +0530

    modified add method

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import json
-
 import mysql.connector
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session
 
 app=Flask(__name__)
+
+
+app.secret_key = 'albin@democracy@123'
 # Connect to MySQL using mysql.connector
 mydb = mysql.connector.connect(
         host="localhost",
@@ -37,7 +38,7 @@ def convertJsonyfy(random_cafe):
 
 @app.route("/")
 def home():
-    return  render_template('index.html')
+    return  render_template('post.html')
 
 
 @app.route("/random", methods=['GET'])
@@ -89,11 +90,62 @@ def search():
         return  jsonify({"error":"location not founnd",
                          "code":404})
 
-@app.route("/process_data", methods=["POST","GET"])
+
+@app.route("/add", methods=["POST"])
 def post():
-    if request.method=="POST":
-        res=request.form
-        return render_template("index.html", result=res)
+    # Retrieve values from the submitted form
+    id=request.form.get('id')
+    name = request.form.get('name')
+    map_url = request.form.get('map_url')
+    img_url = request.form.get('img_url')
+    location = request.form.get('loc')
+    sockets = bool(request.form.get('sockets'))
+    toilet = bool(request.form.get('toilet'))
+    wifi = bool(request.form.get('wifi'))
+    calls = bool(request.form.get('calls'))
+    seats = request.form.get('seats')
+    coffee_price = request.form.get('coffee_price')
+
+    form_data = {
+        "id":id,
+        "name": name,
+        "map_url": map_url,
+        "img_url": img_url,
+        "location": location,
+        "sockets": sockets,
+        "toilet": toilet,
+        "wifi": wifi,
+        "calls": calls,
+        "seats": seats,
+        "coffee_price": coffee_price
+    }
+    session['form_data'] = form_data
+    sql_query = """
+    INSERT INTO cafes (id, name, map_url, img_url, location, has_sockets, has_toilet, has_wifi, can_take_calls, seats, coffee_price)
+    VALUES (%s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    values = (
+        form_data["id"],
+        form_data["name"],
+        form_data["map_url"],
+        form_data["img_url"],
+        form_data["location"],
+        form_data["sockets"],
+        form_data["toilet"],
+        form_data["wifi"],
+        form_data["calls"],
+        form_data["seats"],
+        form_data["coffee_price"]
+    )
+    mycursor.execute(sql_query, values)
+    mydb.commit()
+
+    return redirect(url_for('details'))
+
+@app.route("/print")
+def details():
+    form_data = session.get('form_data')
+    return jsonify(form_data)
 
 
 
